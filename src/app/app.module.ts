1eca16721c89bfc9dc622fcc00673cb7373ca65d
Pixel-0 <tiny0pixel@gmail.com>
2019-07-11 11:52:22 +0300

    add service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
@@ -9,7 +8,11 @@ import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
-
+import { from } from 'rxjs';
+import {AlertsService} from './alert-service/alerts.service'
+import { HttpClientModule} from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,15 +20,20 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
     GoalDetailComponent,
     StrikethroughDirective,
     DateCountPipe,
-    GoalFormComponent
+    GoalFormComponent,
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
-  ],
+    FormsModule,
+    HttpClientModule,
+    NgProgressModule,// normal progress bar
+    NgProgressHttpModule // progress bar to load http requests
 
-  providers: [],
+  ],
+  providers: [AlertsService],//Add service to providers
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/goals/goal.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Goals } from "../goals";
+
+@Injectable({
+  providedIn: 'root' // we declare that this service should be created by the root application injector
+})
+  export class GoalService{
+  getGoals(){
+    return Goals;
+  }
+  constructor() { }
+}
